ac98f7c43ea39b616d7feb9abfee658e527f4368
ThomasBlichfeldt2409 <Thomas.Blichfeldt@specialisterne.dk>
2026-03-16 19:20:21 +0100

    HandleInput method to Slime.cs

--- DungeonSlime/GameObjects/Slime.cs
+++ DungeonSlime/GameObjects/Slime.cs
@@ -61,5 +61,35 @@ namespace DungeonSlime.GameObjects
             // Zero out the movement timer
             _movementTimer = TimeSpan.Zero;
         }
+
+        private void HandleInput()
+        {
+            Vector2 potentialNextDirection = _nextDirection;
+
+            if (GameController.MoveUp())
+            {
+                potentialNextDirection = -Vector2.UnitY;
+            }
+            else if (GameController.MoveDown())
+            {
+                potentialNextDirection = Vector2.UnitY;
+            }
+            else if (GameController.MoveLeft())
+            {
+                potentialNextDirection = -Vector2.UnitX;
+            }
+            else if (GameController.MoveRight())
+            {
+                potentialNextDirection = Vector2.UnitX;
+            }
+
+            // Only allow direction change if it is not reversing the current
+            // direction. This prevents the slime from backing into itself
+            float dot = Vector2.Dot(potentialNextDirection, _segments[0].Direction);
+            if (dot >= 0)
+            {
+                _nextDirection = potentialNextDirection;
+            }
+        }
     }
 }
