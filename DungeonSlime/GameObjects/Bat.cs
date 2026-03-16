46555721ffa9ad82ed3b73ae94e9e48c1d3880f8
TheSharpKillers <ThomasBN2409@gmail.com>
2026-03-16 20:19:07 +0100

    Create Bat.cs

---
+++ DungeonSlime/GameObjects/Bat.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using MonoGameLibrary;
+using MonoGameLibrary.Graphics;
+
+namespace DungeonSlime.GameObjects
+{
+    public class Bat
+    {
+    }
+}
