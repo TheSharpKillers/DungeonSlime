d33b0444242ec4e1701d86a4456cbfab6ca09212
ThomasBlichfeldt2409 <Thomas.Blichfeldt@specialisterne.dk>
2026-03-16 19:00:43 +0100

    Fields and Events for Slime.cs

--- DungeonSlime/GameObjects/Slime.cs
+++ DungeonSlime/GameObjects/Slime.cs
@@ -7,6 +7,30 @@ namespace DungeonSlime.GameObjects
 {
     public class Slime
     {
+        // A constant value that represent the amount of time to wait between movment updates.
+        private static readonly TimeSpan s_movementTime = TimeSpan.FromMilliseconds(200);
 
+        // The amount of time that has elapsed since the last movement update.
+        private TimeSpan _movementTimer;
+
+        // Normalized value (0-1) representing progress between movement ticks for visual interpolation.
+        private float _movementProgress;
+
+        // The next direction to apply to the head of the slime chain during the next movement update.
+        private Vector2 _nextDirection;
+
+        // The number of pixels to move the head segment during the movement cycle.
+        private float _stride;
+
+        // Tracks the segments of the slime chain.
+        private List<SlimeSegment> _segments;
+
+        // The AnimatedSprite used when drawing each slime segment.
+        private AnimatedSprite _sprite;
+
+        /// <summary>
+        /// Event that is raised if it is detected that the head segment of the slime has collided with a body segment.
+        /// </summary>
+        public event EventHandler BodyCollision;
     }
 }
