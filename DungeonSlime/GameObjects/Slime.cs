62dfef184835f9c54c7181fc3b39567d40adda12
ThomasBlichfeldt2409 <Thomas.Blichfeldt@specialisterne.dk>
2026-03-16 19:51:46 +0100

    Update method for Slime.cs

--- DungeonSlime/GameObjects/Slime.cs
+++ DungeonSlime/GameObjects/Slime.cs
@@ -151,5 +151,32 @@ namespace DungeonSlime.GameObjects
             // Add the new tail segment
             _segments.Add(newTail);
         }
+
+        /// <summary>
+        /// Updates the slime.
+        /// </summary>
+        /// <param name="gameTime">A snapshot of the timing values for the current update cycle.</param>
+        public void Update(GameTime gameTime)
+        {
+            // Update the animated sprite
+            _sprite.Update(gameTime);
+
+            // Handle any payer input
+            HandleInput();
+
+            // Increment the movement timer by the frame elapsed time
+            _movementTimer += gameTime.ElapsedGameTime;
+
+            // if the movement timer has accumulated enough time to be greater than
+            // the movement time threshold, then perform a full movement.
+            if (_movementTimer >= s_movementTime)
+            {
+                _movementTimer -= s_movementTime;
+                Move();
+            }
+
+            // Update the movement lerp offset amount
+            _movementProgress = (float)(_movementTimer.TotalSeconds / s_movementTime.TotalSeconds);
+        }
     }
 }
