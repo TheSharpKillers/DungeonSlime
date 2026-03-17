dff3a0338e463472bc6044e4e5a936f27bf4cd60
TheSharpKillers <ThomasBN2409@gmail.com>
2026-03-17 08:08:35 +0100

    Fields and Properties for Bat.cs

--- DungeonSlime/GameObjects/Bat.cs
+++ DungeonSlime/GameObjects/Bat.cs
@@ -7,5 +7,21 @@ namespace DungeonSlime.GameObjects
 {
     public class Bat
     {
+        private const float MOVEMENT_SPEED = 5.0f;
+
+        // The velocity of the bat that defines the direction and how much in that
+        // direction to update the bats position each update cycle.
+        private Vector2 _velocity;
+
+        // The AmimatedSprite used when drawing the bat.
+        private AnimatedSprite _sprite;
+
+        // The sound effect to play when the bat bounces off the edge of the room.
+        private SoundEffect _bounceSoundEffect;
+
+        /// <summary>
+        /// Gets or Sets the position of the bat.
+        /// </summary>
+        public Vector2 Position { get; set; }
     }
 }
